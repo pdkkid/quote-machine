fae0243c82f86a7e0dfdca92a9d95c3f7edfe473
austinterry <pdkkid1@gmail.com>
2021-03-23 13:12:57 -0600

    Added link to source code, modified styling, again...

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import styles from './app.module.scss'
 
 ReactDOM.render(
   <React.StrictMode>
       <RecoilRoot>
         <App />
+        <Source />
       </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+function Source() {
+  return (
+    <div className={styles.source}>
+      <a href="https://github.com/pdkkid/quote-machine">Source</a>
+    </div>
+  );
+}
