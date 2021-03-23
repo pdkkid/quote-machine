8d3486aaf1199a5d905743ad7d756ac31387969c
austinterry <pdkkid1@gmail.com>
2021-03-22 23:17:24 -0600

    refactor state usage and fetching implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,48 @@
-import React, { useEffect } from "react";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import React, { Suspense, useCallback, useState } from "react";
+import { useRecoilValue} from "recoil";
 import styles from "./app.module.scss";
-import { allQuotesState, quoteState } from "./atoms";
+import { allQuotesSelector, Quote } from "./state";
 
 function App() {
-  const setAllQuotes = useSetRecoilState(allQuotesState);
-
-  useEffect(() => {
-    const getAllQuotes = async () => {
-      const url =
-        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
-      const resp = await fetch(url);
-      const body = await resp.json();
-      setAllQuotes(body["quotes"]);
-    };
-    getAllQuotes();
-  });
-
   return (
     <div className={styles.quoteContainer}>
-      <h1>Random Quote Machine</h1>
-      <QuoteBox />
+      <span className={styles.title}>Random Quote Machine</span>
+        <Suspense fallback={<span className={styles.loading}>Loading...</span>}>
+          <QuoteBox />
+        </Suspense>
     </div>
   );
 }
 
 function QuoteBox() {
-  const [quote, setQuote] = useRecoilState(quoteState);
-  const allQuotes = useRecoilValue(allQuotesState);
-  let rand: number;
+  const allQuotes = useRecoilValue(allQuotesSelector);
 
+  const newQuote = useCallback(():Quote => {
+    let rand = Math.floor(Math.random() * allQuotes.length);
+      return allQuotes[rand];
+  }, [allQuotes]);
+
+  const [quote, setQuote] = useState<Quote>(newQuote());
+  
   const doTweet = () => {
     console.log("Did the Tweet");
   };
 
-  const newQuote = () => {
-    rand = Math.floor(Math.random() * allQuotes.length);
-    if (allQuotes.length > 0)
-      setQuote({
-        text: allQuotes[rand].quote,
-        author: allQuotes[rand].author,
-      });
-  };
-
-  if (!quote.text || !quote.author) {
-    newQuote()
-  }
+  const handleNewQuote = useCallback(() => {
+    setQuote(newQuote())
+  }, [setQuote, newQuote])
 
   return (
     <div className={styles.quoteBody}>
-      <div className={styles.quoteText + " " + styles.fadein}>
-        {quote.text}
+      <div className={styles.quoteText}>
+        {quote.quote}
         <div>- {quote.author}</div>
       </div>
       <div className={styles.buttonContainer}>
         <button onClick={doTweet}>
           Tweet
         </button>
-        <button onClick={newQuote}>
+        <button onClick={handleNewQuote}>
           New Quote
         </button>
       </div>
---
+++ src/state.tsx
@@ -0,0 +1,24 @@
+import { waitFor } from "@testing-library/dom";
+import { selector } from "recoil";
+
+// List of quotes interface
+export interface Quote {
+  quote: string;
+  author: string;
+}
+
+const sleep = (milliseconds: number) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+export const allQuotesSelector = selector<Quote[]>({
+  key: 'allQuotes',
+  get: async () => {
+    const response = await fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    );
+    const body = await response.json();
+    await sleep(10000)
+    return body["quotes"];
+  },
+});
